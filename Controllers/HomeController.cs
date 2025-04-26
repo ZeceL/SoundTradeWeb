1441906acba292c54ed9e817574c1a65b8accd79
ZeceL <achernysev@gmail.com>
2025-04-26 16:13:19 +0300

    обновлённая главная страница

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore; // Для доступа к БД
 using SoundTradeWebApp.Data;       // Контекст БД
 using SoundTradeWebApp.Models;      // Для ErrorViewModel
-using SoundTradeWebApp.Models.ViewModels; // Для TrackIndexViewModel
+using SoundTradeWebApp.Models.ViewModels; // Для TrackIndexViewModel и IndexPageViewModel
 using System.Diagnostics;
 using System.Linq;                 // Для .OrderByDescending, .Take, .Select
 using System.Threading.Tasks;      // Для Task<>
+using System.Collections.Generic; // Для IEnumerable
 
 namespace SoundTradeWebApp.Controllers
 {
@@ -24,13 +25,15 @@ namespace SoundTradeWebApp.Controllers
         // Обновляем метод Index
         public async Task<IActionResult> Index()
         {
-            // Загружаем N последних треков для раздела "Рекомендуем"
-            int numberOfRecommendedTracks = 6; // Сколько треков показывать
+            int numberOfTracks = 6; // Сколько треков показывать в каждой секции
 
-            var recommendedTracks = await _context.Tracks
+            var viewModel = new IndexPageViewModel();
+
+            // Загружаем N последних треков для раздела "Рекомендуем"
+            viewModel.RecommendedTracks = await _context.Tracks
                                         .AsNoTracking() // Только для чтения
                                         .OrderByDescending(t => t.UploadDate) // Сортируем по дате (сначала новые)
-                                        .Take(numberOfRecommendedTracks)       // Берем первые N
+                                        .Take(numberOfTracks)       // Берем первые N
                                         .Select(t => new TrackIndexViewModel // Проецируем в ViewModel
                                         {
                                             Id = t.Id,
@@ -43,8 +46,89 @@ namespace SoundTradeWebApp.Controllers
                                         })
                                         .ToListAsync();
 
-            // Передаем список рекомендованных треков в представление
-            return View(recommendedTracks);
+            // Загружаем N последних треков для раздела "Поп музыка"
+            viewModel.PopTracks = await _context.Tracks
+                                    .AsNoTracking()
+                                    .Where(t => t.Genre == "Pop") // Фильтруем по жанру "Поп"
+                                    .OrderByDescending(t => t.UploadDate)
+                                    .Take(numberOfTracks)
+                                    .Select(t => new TrackIndexViewModel
+                                    {
+                                        Id = t.Id,
+                                        Title = t.Title,
+                                        ArtistName = t.ArtistName,
+                                        Genre = t.Genre,
+                                        AudioFilePath = Url.Action("GetAudio", "Tracks", new { id = t.Id }) ?? ""
+                                    })
+                                    .ToListAsync();
+
+            // Загружаем N последних треков для раздела "Рок музыка"
+            viewModel.RockTracks = await _context.Tracks
+                                    .AsNoTracking()
+                                    .Where(t => t.Genre == "Rock") // Фильтруем по жанру "Рок"
+                                    .OrderByDescending(t => t.UploadDate)
+                                    .Take(numberOfTracks)
+                                    .Select(t => new TrackIndexViewModel
+                                    {
+                                        Id = t.Id,
+                                        Title = t.Title,
+                                        ArtistName = t.ArtistName,
+                                        Genre = t.Genre,
+                                        AudioFilePath = Url.Action("GetAudio", "Tracks", new { id = t.Id }) ?? ""
+                                    })
+                                    .ToListAsync();
+
+            // Загружаем N последних треков для раздела "Хип-хоп музыка"
+            viewModel.HipHopTracks = await _context.Tracks
+                                        .AsNoTracking()
+                                        .Where(t => t.Genre == "HipHop") // Фильтруем по жанру "Хип-хоп"
+                                        .OrderByDescending(t => t.UploadDate)
+                                        .Take(numberOfTracks)
+                                        .Select(t => new TrackIndexViewModel
+                                        {
+                                            Id = t.Id,
+                                            Title = t.Title,
+                                            ArtistName = t.ArtistName,
+                                            Genre = t.Genre,
+                                            AudioFilePath = Url.Action("GetAudio", "Tracks", new { id = t.Id }) ?? ""
+                                        })
+                                        .ToListAsync();
+
+            // Загружаем N последних треков для раздела "Джаз музыка"
+            viewModel.JazzTracks = await _context.Tracks
+                                    .AsNoTracking()
+                                    .Where(t => t.Genre == "Jazz") // Фильтруем по жанру "Джаз"
+                                    .OrderByDescending(t => t.UploadDate)
+                                    .Take(numberOfTracks)
+                                    .Select(t => new TrackIndexViewModel
+                                    {
+                                        Id = t.Id,
+                                        Title = t.Title,
+                                        ArtistName = t.ArtistName,
+                                        Genre = t.Genre,
+                                        AudioFilePath = Url.Action("GetAudio", "Tracks", new { id = t.Id }) ?? ""
+                                    })
+                                    .ToListAsync();
+
+            // Загружаем N последних треков для раздела "Электронная музыка"
+            viewModel.ElectronicTracks = await _context.Tracks
+                                            .AsNoTracking()
+                                            .Where(t => t.Genre == "Electronic") // Фильтруем по жанру "Электронная музыка"
+                                            .OrderByDescending(t => t.UploadDate)
+                                            .Take(numberOfTracks)
+                                            .Select(t => new TrackIndexViewModel
+                                            {
+                                                Id = t.Id,
+                                                Title = t.Title,
+                                                ArtistName = t.ArtistName,
+                                                Genre = t.Genre,
+                                                AudioFilePath = Url.Action("GetAudio", "Tracks", new { id = t.Id }) ?? ""
+                                            })
+                                            .ToListAsync();
+
+
+            // Передаем ViewModel с данными всех секций в представление
+            return View(viewModel);
         }
 
         // Метод Error остается без изменений
---
+++ Models/ViewModels/IndexPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SoundTradeWebApp.Models.ViewModels
+{
+    public class IndexPageViewModel
+    {
+        public IEnumerable<TrackIndexViewModel> RecommendedTracks { get; set; }
+        public IEnumerable<TrackIndexViewModel> PopTracks { get; set; }
+        public IEnumerable<TrackIndexViewModel> RockTracks { get; set; }
+        public IEnumerable<TrackIndexViewModel> HipHopTracks { get; set; }
+        public IEnumerable<TrackIndexViewModel> JazzTracks { get; set; }
+        public IEnumerable<TrackIndexViewModel> ElectronicTracks { get; set; }
+    }
+}
